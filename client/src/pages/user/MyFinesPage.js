1b547659858995896efdff2836cbb2268663a827
cattuongxp84 <hocappscript@gmail.com>
2026-04-23 06:50:44 +0700

    Update MyFinesPage.js

--- client/src/pages/user/MyFinesPage.js
+++ client/src/pages/user/MyFinesPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FiAlertTriangle, FiBook, FiClock } from 'react-icons/fi';
+import { FiAlertTriangle, FiClock } from 'react-icons/fi';
 import Layout from '../../components/common/Layout';
 import api from '../../services/api';
 
